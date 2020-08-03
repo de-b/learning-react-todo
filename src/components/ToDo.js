6347ec843c25117a9afe29d04ba411400f0e15cc
ayrbox <srdx7@hotmail.com>
2020-08-03 23:23:03 +0100

    Generate UUID and add other item information on add.

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,55 +1,63 @@
-import React, { useState } from "react";
-import ToDoForm from "./ToDoForm";
-import ListOfTodos from "./ListOfTodos";
-import { v4 as uuidv4 } from "uuid";
+import React, { useState } from 'react';
+import ToDoForm from './ToDoForm';
+import ListOfTodos from './ListOfTodos';
+import { v4 as uuidv4 } from 'uuid';
 
-import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
-import Card from "@material-ui/core/Card";
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
 const mainTodos = [
   {
-    category: "shopping",
+    category: 'shopping',
     id: uuidv4(),
     completed: false,
-    task: "buy and eggs",
+    task: 'buy and eggs',
   },
   {
-    category: "shopping",
+    category: 'shopping',
     id: uuidv4(),
     completed: false,
-    task: "buy and oranges test",
+    task: 'buy and oranges test',
   },
 ];
 
 const ToDo = () => {
   const [todos, setTodos] = useState(mainTodos);
-  const [inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState('');
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setInputText(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (inputText === "") return;
-    setTodos([...todos, { task: inputText }]);
-    setInputText("");
+    if (inputText === '') return;
+    setTodos([
+      ...todos,
+      {
+        category: 'shopping',
+        id: uuidv4(),
+        task: inputText,
+        completed: false,
+      },
+    ]);
+    setInputText('');
   };
 
   const clearItems = () => {
     setTodos([]);
   };
 
-  const deleteItem = (id) => {
-    let temp = todos.filter((item) => item.id !== id);
+  const deleteItem = id => {
+    let temp = todos.filter(item => item.id !== id);
     setTodos(temp);
   };
 
   return (
     <Container maxWidth="sm">
       <Box m={3}>
-        <Card style={{ padding: "25px" }}>
+        <Card style={{ padding: '25px' }}>
           <Typography variant="h5" color="initial">
             Your Todo lists
           </Typography>
